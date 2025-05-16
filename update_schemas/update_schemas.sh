4996009b35ecf1581bd8b453bd1f5d6d3f33de13
Fábio Ferreira <fabioferreira@edu.ulisboa.pt>
2025-05-16 11:56:29 +0100

    improvement: improve update_schemas.sh to use, by default, a virtual environment (venv)

--- update_schemas/update_schemas.sh
+++ update_schemas/update_schemas.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+python3 -m venv .venv &&
+source .venv/bin/activate &&
 pip install -r ${SCRIPT_DIR}/requirements.txt &&
 python3 ${SCRIPT_DIR}/update_schemas.py
